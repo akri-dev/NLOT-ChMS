7f13db3addc7597a9f811aaa065ced0c3c916c86
Akri <alecjoseph.dev@gmail.com>
2025-07-27 23:03:48 +0800

    modified initial migration schema - added profile id

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,8 +19,9 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->unsignedBigInteger('role_id')
-                    ->default(2)
+                    ->default(4)
                     ->comment('1:system-admin 2:church-staff 3:pastors 4:church-members');
+            $table->unsignedBigInteger('profile_id');
             $table->timestamps();
         });
 
