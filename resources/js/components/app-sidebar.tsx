4a3583c3f557ebfe5a60dc051b99ba85638891d7
Akri <alecjoseph.dev@gmail.com>
2025-08-01 01:22:14 +0800

    added views for User Management

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -7,26 +7,7 @@ import { Link, usePage } from '@inertiajs/react';
 import { BookOpen, Folder, LayoutGrid, Users } from 'lucide-react';
 import AppLogo from './app-logo';
 
-const mainNavItemsGeneral: NavItem[] = [
-    {
-        title: 'Dashboard',
-        href: '/dashboard',
-        icon: LayoutGrid,
-    },
-];
 
-const mainNavItemsSystem: NavItem[] = [
-    {
-        title: 'Dashboard',
-        href: '/dashboard',
-        icon: LayoutGrid,
-    },
-    {
-        title: 'User Management',
-        href: '/user-management',
-        icon: Users,
-    },
-];
 
 const footerNavItems: NavItem[] = [
     {
@@ -46,59 +27,48 @@ export function AppSidebar() {
 
     const currentRole = auth.user.role_name;
 
-    const systemAdmin = currentRole === 'system-admin';
+    const mainNavItems = currentRole === 'system-admin' ? [
+        {
+            title: 'Dashboard',
+            href: '/dashboard',
+            icon: LayoutGrid,
+        },
+        {
+            title: 'User Management',
+            href: '/admin/users',
+            icon: Users,
+        },
+    ] : [
+            {
+                title: 'Dashboard',
+                href: '/dashboard',
+                icon: LayoutGrid,
+            },
+        ];
 
-    if (systemAdmin) {
-        return (
-            <Sidebar collapsible="icon" variant="inset">
-                <SidebarHeader>
-                    <SidebarMenu>
-                        <SidebarMenuItem>
-                            <SidebarMenuButton size="lg" asChild>
-                                <Link href="/dashboard" prefetch>
-                                    <AppLogo />
-                                </Link>
-                            </SidebarMenuButton>
-                        </SidebarMenuItem>
-                    </SidebarMenu>
-                </SidebarHeader>
+    return (
+        <Sidebar collapsible="icon" variant="inset">
+            <SidebarHeader>
+                <SidebarMenu>
+                    <SidebarMenuItem>
+                        <SidebarMenuButton size="lg" asChild>
+                            <Link href="/dashboard" prefetch>
+                                <AppLogo />
+                            </Link>
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarHeader>
 
-                <SidebarContent>
-                    <NavMain items={mainNavItemsSystem} />
-                </SidebarContent>
+            <SidebarContent>
+                <NavMain items={mainNavItems} />
+            </SidebarContent>
 
-                <SidebarFooter>
-                    <NavFooter items={footerNavItems} className="mt-auto" />
-                    <NavUser />
-                </SidebarFooter>
-            </Sidebar>
-        );
-    } else {
-        return (
-            <Sidebar collapsible="icon" variant="inset">
-                <SidebarHeader>
-                    <SidebarMenu>
-                        <SidebarMenuItem>
-                            <SidebarMenuButton size="lg" asChild>
-                                <Link href="/dashboard" prefetch>
-                                    <AppLogo />
-                                </Link>
-                            </SidebarMenuButton>
-                        </SidebarMenuItem>
-                    </SidebarMenu>
-                </SidebarHeader>
-
-                <SidebarContent>
-                    <NavMain items={mainNavItemsGeneral} />
-                </SidebarContent>
-
-                <SidebarFooter>
-                    <NavFooter items={footerNavItems} className="mt-auto" />
-                    <NavUser />
-                </SidebarFooter>
-            </Sidebar>
-        );
-    }
-
+            <SidebarFooter>
+                <NavFooter items={footerNavItems} className="mt-auto" />
+                <NavUser />
+            </SidebarFooter>
+        </Sidebar>
+    );
 }
 
--- resources/js/pages/user-management.tsx
+++ resources/js/pages/user-management.tsx
@@ -1,35 +1,110 @@
-import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
+"use client"
+
+import * as React from "react"
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
+import { Button } from "@/components/ui/button";
+import { ChevronDown } from 'lucide-react';
+
+
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuRadioGroup,
+    DropdownMenuRadioItem,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
         title: 'User Management',
-        href: '/user-management',
+        href: '/admin/users',
     },
 ];
 
-export default function userManagement() {
+export default function userManagement({ users }) {
+
+    const [position, setPosition] = React.useState("bottom")
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="User Management" />
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto">
-                <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-                    <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-                    </div>
-                    <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-                    </div>
-                    <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-                    </div>
-                </div>
-                <div className="relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
-                    <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-                </div>
+                <Table>
+                    <TableCaption>A list of registered users</TableCaption>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead>ID</TableHead>
+                            <TableHead>Username</TableHead>
+                            <TableHead>Email</TableHead>
+                            <TableHead>Joined Date</TableHead>
+                            <TableHead>System Privileges</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        {users.map((user) => (
+                            <TableRow key={user.id}>
+                                <TableCell className="font-medium">{user.id}</TableCell>
+                                <TableCell>{user.username}</TableCell>
+                                <TableCell>{user.email}</TableCell>
+                                <TableCell>{new Date(user.created_at).toLocaleDateString('en-US', {
+                                    year: 'numeric',
+                                    month: 'long',
+                                    day: 'numeric'
+                                })}</TableCell>
+                                <TableCell>
+                                    <DropdownMenu>
+                                        <DropdownMenuTrigger>
+                                            <Button variant="outline">
+                                                {user.role
+                                                    ? user.role.role_name === 'system-admin'
+                                                        ? 'System Administrator'
+                                                        : user.role.role_name === 'ministry-leader'
+                                                            ? 'Ministry Leader'
+                                                            : user.role.role_name === 'ministry-staff'
+                                                                ? 'Ministry Leader'
+                                                                : 'Church Member '
+                                                    : 'No Role Assigned'}
+                                                <ChevronDown />
+                                            </Button>
+                                        </DropdownMenuTrigger>
+                                        <DropdownMenuContent>
+                                            <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
+                                                <DropdownMenuRadioItem value="top">Top</DropdownMenuRadioItem>
+                                                <DropdownMenuRadioItem value="bottom">Bottom</DropdownMenuRadioItem>
+                                                <DropdownMenuRadioItem value="right">Right</DropdownMenuRadioItem>
+                                            </DropdownMenuRadioGroup>
+                                        </DropdownMenuContent>
+                                    </DropdownMenu>
+                                </TableCell>
+
+
+                                <TableCell>
+                                    <Button variant="outline" className='mr-1 bg-blue-600 text-stone-50 hover:cursor-pointer'>Edit</Button>
+                                    <Button variant="outline" className='bg-red-500 text-stone-50 hover:cursor-pointer'>Delete</Button>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                        {users.length === 0 && (
+                            <TableRow>
+                                <TableCell colSpan={4} className="text-center">
+                                    No users found.
+                                </TableCell>
+                            </TableRow>
+                        )}
+                    </TableBody>
+                </Table>
             </div>
-        </AppLayout>
+        </AppLayout >
     );
 }
